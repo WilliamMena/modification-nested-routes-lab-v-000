1a62e755bc5058ef0b31a3778a25463ff6a77f2e
WilliamMena <williammena1991@gmail.com>
2017-02-08 00:42:42 -0500

    Done.

--- app/helpers/artists_helper.rb
+++ app/helpers/artists_helper.rb
@@ -2,4 +2,16 @@ module ArtistsHelper
   def display_artist(song)
     song.artist.nil? ? link_to("Add Artist", edit_song_path(song)) : link_to(song.artist_name, artist_path(song.artist))
   end
+
+  def artist_select(song, artist)
+    if song.artist.nil?
+      #i need to display the artist name if they are editing through the nested route
+      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
+
+    elsif @nested
+      "Artist: #{song.artist.name}"
+    else
+      hidden_field_tag "song[artist_id]", song.artist_id
+    end
+  end
 end
--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,14 +1,4 @@
 module SongsHelper
-  def artist_select(song, artist)
-    if song.artist.nil?
-      #i need to display the artist name if they are editing through the nested route
-      select_tag "song[artist_id]", options_from_collection_for_select(Artist.all, :id, :name)
 
-    elsif !artist.nil?
-      "Artist: #{artist.name}"
-    else
-      hidden_field_tag "song[artist_id]", song.artist_id
-    end
-  end
 
 end
